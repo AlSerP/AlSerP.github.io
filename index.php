87a89314bd13855e48c9e378d949cc278bf528f4
AlSerP <gosha1416@yandex.ru>
2017-08-07 10:53:01 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Document</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css?family=Sedgwick+Ave');
+        h1 {
+            color: red;
+            font-family: 'Sedgwick Ave', cursive;
+        }
+
+    </style>
+</head>
+
+<body>
+    <p>George</p>
+     <?php
+     echo "<h1>1</h1>";
+     ?>
+</body>
+
+</html>
